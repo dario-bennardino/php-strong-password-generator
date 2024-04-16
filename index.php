8edb1f8c9b33df02726bfc1828ea770723021b8b
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-16 14:13:55 +0200

    [add] inizio esercizio

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="password">
+        <button type="submit">Genera</button>
+    </form>
+</body>
+
+</html>
