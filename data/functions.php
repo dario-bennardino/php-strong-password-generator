9dc4dcfdd4d753632e9e54a9ffc51b51286508bc
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-16 17:56:35 +0200

    [add] milestone II completa

---
+++ data/functions.php
@@ -0,0 +1,28 @@
+<?php
+function generate_password($length)
+{
+    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:"<>?-=[]\;,./';
+    $password = '';
+    $lunghezza_password = strlen($caratteri) - 1;
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $caratteri[mt_rand(0, $lunghezza_password)];
+    }
+
+    return $password;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Verifico se è stata specificata la lunghezza della password
+    if (isset($_POST['lunghezza_password'])) {
+        //ottengo la lunghezza della password e la genero
+        $lunghezza = intval($_POST['lunghezza_password']);
+        $password_generata = generate_password($lunghezza);
+    }
+}
+
+// Verifica se è stato inviato il modulo con il pulsante Reset
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reset'])) {
+    // Resetta la variabile della password generata
+    $password_generata = '';
+}
--- index.php
+++ index.php
@@ -1,32 +1,8 @@
 <?php
-function generate_password($length)
-{
-    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:"<>?-=[]\;,./';
-    $password = '';
-    $lunghezza_password = strlen($caratteri) - 1;
-
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $caratteri[mt_rand(0, $lunghezza_password)];
-    }
-
-    return $password;
-}
+require_once __DIR__ . '/data/functions.php'
 
 // Verifico se è stata inviata una richiesta POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //Verifico se è stata specificata la lunghezza della password
-    if (isset($_POST['lunghezza_password'])) {
-        //ottengo la lunghezza della password e la genero
-        $lunghezza = intval($_POST['lunghezza_password']);
-        $password_generata = generate_password($lunghezza);
-    }
-}
 
-// Verifica se è stato inviato il modulo con il pulsante Reset
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reset'])) {
-    // Resetta la variabile della password generata
-    $password_generata = '';
-}
 
 ?>
 
@@ -38,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reset'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -46,20 +22,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reset'])) {
 <body>
 
     <div class="container my-4">
-        <h1>PHP Strong Password Generator</h1>
+        <h1 class="text-center p-4 ">PHP Strong Password Generator</h1>
 
         <form action="index.php" method="POST" class="mt-4">
 
             <label for="lunghezza_password"><strong>Lunghezza Password</strong></label>
             <input type="number" class="form-control" id="lunghezza_password" name="lunghezza_password">
-            <button type="submit" class="btn btn-primary">Genera</button>
-            <button type="submit" class="btn btn-secondary" name="reset">Reset</button>
+            <div class="buttons mt-4 ">
+                <button type="submit" class="btn btn-primary">Genera</button>
+                <button type="submit" class="btn btn-secondary" name="reset">Reset</button>
+            </div>
+
 
         </form>
 
         <!-- stampo la password solo se è stata generata -->
         <?php if (isset($password_generata)) : ?>
             <h4>La password generata è: <?php echo $password_generata; ?> </h4>
+
         <?php endif; ?>
     </div>
 
